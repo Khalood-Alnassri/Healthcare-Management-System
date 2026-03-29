a5f118c0548147e0455aedaaeb94cd0197041890
Khalood.kk <alnasseikallood@gmail.com>
2026-03-29 09:49:36 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Healthcare_Management_System
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] patientNames = new string[100];
+            string[] patientIDs = new string[100];
+            string[] diagnoses = new string[100];
+            bool[] admitted = new bool[100];       // true = currently admitted
+            string[] assignedDoctors = new string[100];
+            string[] departments = new string[100];     // e.g. "Cardiology", "Orthopedics"
+            int[] visitCount = new int[100];        // how many times admitted
+            double[] billingAmount = new double[100];     // total fees owed
+            int lastPatientIndex = -1;
+
+            //Data storage
+           
+            //Patient 1
+
+            patientNames[lastPatientIndex] = "Ali Hassan";
+            patientIDs [lastPatientIndex] = "P001";
+            diagnoses [lastPatientIndex] = "Flu";
+            admitted [lastPatientIndex] = false;
+            assignedDoctors [lastPatientIndex] = " ";
+            departments [lastPatientIndex] = "General";
+            visitCount [lastPatientIndex] = 2;
+            billingAmount [lastPatientIndex] = 0;
+            lastPatientIndex++;
+
+            //Patient 2
+
+            patientNames[lastPatientIndex] = "Sara Ahmed";
+            patientIDs[lastPatientIndex] = "P002";
+            diagnoses[lastPatientIndex] = "Fracture";
+            admitted[lastPatientIndex] = true;
+            assignedDoctors[lastPatientIndex] = "Dr. Noor";
+            departments[lastPatientIndex] = "Orthopedics";
+            visitCount[lastPatientIndex] = 4;
+            billingAmount[lastPatientIndex] = 0;
+            lastPatientIndex++;
+
+            //Patient 3
+
+            patientNames[lastPatientIndex] = "Omar Khalid";
+            patientIDs[lastPatientIndex] = "P003";
+            diagnoses[lastPatientIndex] = "Diabetes";
+            admitted[lastPatientIndex] = false;
+            assignedDoctors[lastPatientIndex] = " ";
+            departments[lastPatientIndex] = "Cardiology";
+            visitCount[lastPatientIndex] = 1;
+            billingAmount[lastPatientIndex] = 0;
+            lastPatientIndex++;
+
+
+
+        }
+    }
+}
